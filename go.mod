efb4cbf5dc77776917e6cc44eccf0d7c8d637b6a
jay-testbook <jay.kalokar@testbook.com>
2022-06-08 15:54:37 +0530

    Add last updated on time

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/rwynn/gtm v1.0.1-0.20191119151623-081995b34c9c
 	github.com/serialx/hashring v0.0.0-20200727003509-22c0c7ab6b1b // indirect
-	github.com/testbook/app-search-client v0.0.0-20220523083047-c4593b64bda7 // indirect
+	github.com/testbook/app-search-client v0.0.0-20220523083047-c4593b64bda7
 	go.mongodb.org/mongo-driver v1.8.4
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- stats.go
+++ stats.go
@@ -1,15 +1,18 @@
 package main
 
+import "time"
+
 type bulkProcessorStats struct {
-	Enabled   bool
-	Flushed   int64 // number of times the flush interval has been invoked
-	Committed int64 // # of times workers committed bulk requests
-	Indexed   int64 // # of requests indexed
-	Created   int64 // # of requests that ES reported as creates (201)
-	Updated   int64 // # of requests that ES reported as updates
-	Deleted   int64 // # of requests that ES reported as deletes
-	Succeeded int64 // # of requests that ES reported as successful
-	Failed    int64 // # of requests that ES reported as failed
+	Enabled      bool
+	Flushed      int64 // number of times the flush interval has been invoked
+	Committed    int64 // # of times workers committed bulk requests
+	Indexed      int64 // # of requests indexed
+	Created      int64 // # of requests that ES reported as creates (201)
+	Updated      int64 // # of requests that ES reported as updates
+	Deleted      int64 // # of requests that ES reported as deletes
+	Succeeded    int64 // # of requests that ES reported as successful
+	Failed       int64 // # of requests that ES reported as failed
+	LastUpdateTs time.Time
 }
 
 func (s *bulkProcessorStats) AddFlushed(c int) {
@@ -17,6 +20,7 @@ func (s *bulkProcessorStats) AddFlushed(c int) {
 		return
 	}
 	s.Flushed += int64(c)
+	s.LastUpdateTs = time.Now()
 }
 
 func (s *bulkProcessorStats) AddCommitted(c int) {
@@ -24,6 +28,7 @@ func (s *bulkProcessorStats) AddCommitted(c int) {
 		return
 	}
 	s.Committed += int64(c)
+	s.LastUpdateTs = time.Now()
 }
 
 func (s *bulkProcessorStats) AddIndexed(c int) {
@@ -31,6 +36,7 @@ func (s *bulkProcessorStats) AddIndexed(c int) {
 		return
 	}
 	s.Indexed += int64(c)
+	s.LastUpdateTs = time.Now()
 }
 
 func (s *bulkProcessorStats) AddCreated(c int) {
@@ -38,6 +44,7 @@ func (s *bulkProcessorStats) AddCreated(c int) {
 		return
 	}
 	s.Created += int64(c)
+	s.LastUpdateTs = time.Now()
 }
 
 func (s *bulkProcessorStats) AddUpdated(c int) {
@@ -45,6 +52,7 @@ func (s *bulkProcessorStats) AddUpdated(c int) {
 		return
 	}
 	s.Updated += int64(c)
+	s.LastUpdateTs = time.Now()
 }
 
 func (s *bulkProcessorStats) AddDeleted(c int) {
@@ -52,6 +60,7 @@ func (s *bulkProcessorStats) AddDeleted(c int) {
 		return
 	}
 	s.Deleted += int64(c)
+	s.LastUpdateTs = time.Now()
 }
 
 func (s *bulkProcessorStats) AddSucceeded(c int) {
@@ -59,6 +68,7 @@ func (s *bulkProcessorStats) AddSucceeded(c int) {
 		return
 	}
 	s.Succeeded += int64(c)
+	s.LastUpdateTs = time.Now()
 }
 
 func (s *bulkProcessorStats) AddFailed(c int) {
@@ -66,4 +76,5 @@ func (s *bulkProcessorStats) AddFailed(c int) {
 		return
 	}
 	s.Failed += int64(c)
+	s.LastUpdateTs = time.Now()
 }
